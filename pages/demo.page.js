63c009ae1d6a5e11dc162ff207677c1ce226e3da
jimCresswell <jim.cresswell@gmail.com>
2019-10-31 06:42:55 +0000

    Add a page subsection and use it in a test

--- pages/demo.page.js
+++ pages/demo.page.js
@@ -1,10 +1,38 @@
 import { Selector } from 'testcafe';
 
+/**
+ * Shared selectors
+ */
+const label = Selector('label');
+
+/**
+ * Page sub-section description.
+ * @param {String} text Text to locate the feature selector with.
+ */
+class FeatureChoice {
+  constructor (text) {
+    this.label    = label.withText(text);
+    this.checkbox = this.label.find('input[type=checkbox]');
+  }
+}
+
+/**
+ * Primary page description.
+ */
 class Page {
   constructor () {
+    // General page elements.
     this.nameInput = Selector('#developer-name');
     this.submit = Selector('#submit-button');
     this.articleHeader = Selector('.result-content').find('h1');
+
+    // Feature selection elements.
+    // TODO: This is very brittle because it relies on highly specific UI text, switch to CSS or text subset.
+    this.featureList = [
+      new FeatureChoice('Support for testing on remote devices'),
+      new FeatureChoice('Re-using existing JavaScript code for testing'),
+      new FeatureChoice('Easy embedding into a Continuous integration system')
+    ]
   }
 }
 
--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,13 +1,32 @@
 import demoPage from '../pages/demo.page';
 
+/**
+ * Test data.
+ */
 const expectedName = 'P Fry';
 
+/**
+ * Testing the main page.
+ */
 fixture('Actions on the main form')
   .page('http://devexpress.github.io/testcafe/example');
 
-test('Basic user data entry', async t => {
+
+/******
+ * The tests.
+ ******/
+
+test('Basic user data entry.', async t => {
   await t
     .typeText(demoPage.nameInput, expectedName)
     .click(demoPage.submit)
     .expect(demoPage.articleHeader.innerText).contains(expectedName, `Name should contain ${expectedName}`);
+});
+
+test('Checkbox state responsiveness.', async t => {
+  for (const feature of demoPage.featureList) {
+    await t
+      .click(feature.label)
+      .expect(feature.checkbox.checked).ok();
+  }
 });
